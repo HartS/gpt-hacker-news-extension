d2f03aec5995f9bd2578bd7af3b5c7baa56dc7e3
Hart Simha <hmsimha@gmail.com>
2022-12-06 15:08:20 -0800

    014
    continue from the following line of the previous output:
    
    var comments = document.querySelectorAll(".comment-tree .comtr

--- content-script.js
+++ content-script.js
@@ -24,5 +24,19 @@ if (currentURL.indexOf("item?id=") !== -1) {
   }
 
   // Get all the comments on the page
-  var comments = document.querySelectorAll(".comment-tree .comtr
+var comments = document.querySelectorAll(".comment-tree .comtr");
+
+// Loop through the comments
+for (var i = 0; i < comments.length; i++) {
+  // Get the timestamp of the comment
+  var commentTime = new Date(comments[i].querySelector(".age a").getAttribute("title")).getTime();
+
+  // Check if the comment was posted after the user's last visit to the thread
+  if (commentTime > lastVisit) {
+    console.log("Comment was posted after the user's last visit. Highlighting comment.");
+
+    // If so, highlight the comment
+    comments[i].style.backgroundColor = "yellow";
+  }
+}
 
