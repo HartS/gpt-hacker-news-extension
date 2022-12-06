6df87d982bb4ff2f3e17032e19a36ed2f7bb196d
Hart Simha <hmsimha@gmail.com>
2022-12-06 14:59:01 -0800

    011
    What would the full content-script.js look like with those changes made to add debugging output?

--- content-script.js
+++ content-script.js
@@ -1,31 +1,24 @@
 // Get the current URL of the page
+console.log("Current URL: ", window.location.href);
+
 var currentURL = window.location.href;
 
 // Check if the current URL is a comment thread on Hacker News
 if (currentURL.indexOf("item?id=") !== -1) {
+  console.log("URL is a comment thread on Hacker News");
+
   // Get the ID of the comment thread from the URL
   var threadID = currentURL.split("=")[1];
 
   // Check if the user has already visited this thread
   if (localStorage.getItem(threadID) === null) {
+    console.log("User has not visited the thread before");
+
     // If not, set the local storage item for this thread to the current date
     localStorage.setItem(threadID, Date.now());
   } else {
+    console.log("User has visited the thread before. Last visit: ", localStorage.getItem(threadID));
+
     // If the user has visited the thread before, get the timestamp of the last visit
-    var lastVisit = localStorage.getItem(threadID);
-
-    // Loop through all the comments on the page
-    var comments = document.querySelectorAll(".comment-tree .comtr");
-    for (var i = 0; i < comments.length; i++) {
-      // Get the timestamp of the comment
-      var commentTime = comments[i].querySelector(".age a").getAttribute("title");
-      commentTime = new Date(commentTime).getTime();
-
-      // If the comment was posted after the user's last visit, highlight it
-      if (commentTime > lastVisit) {
-        comments[i].style.backgroundColor = "yellow";
-      }
-    }
-  }
-}
+    var lastVisit
 
